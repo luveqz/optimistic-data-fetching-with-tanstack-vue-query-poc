de875f6dcd324b3b18ade7095fa8de14fe25bdb4
Luis Velásquez <luvejo.dev@gmail.com>
2024-08-11 17:37:24 -0500

    build: setup Vue Query

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(VueQueryPlugin);
+
+app.mount("#app");
