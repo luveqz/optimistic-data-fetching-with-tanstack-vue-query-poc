9d920282730475970b8854059af2e02fc8a00574
Luis Velásquez <luvejo.dev@gmail.com>
2024-08-25 15:55:00 -0500

    feat: add basic query + mutation + query invalidation example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQueryClient, useQuery, useMutation } from "@tanstack/vue-query";
+import api from "./api";
+
+// Access QueryClient instance
+const queryClient = useQueryClient();
+
+// Query
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ["books"],
+  queryFn: api.books.list,
+});
+
+// Mutation
+const mutation = useMutation({
+  mutationFn: api.books.add,
+  onSuccess: () => {
+    // Invalidate and refetch
+    queryClient.invalidateQueries({ queryKey: ["books"] });
+  },
+});
+
+function onButtonClick() {
+  mutation.mutate({
+    id: Math.floor(Math.random() * 100),
+    title: "Mouthful of Birds",
+    author: "Samanta Schweblin",
+    publishedYear: 2009,
+  });
+}
+</script>
 
 <template>
-  <div>Hi.</div>
+  <div>
+    <span v-if="isPending">Loading...</span>
+    <span v-else-if="isError">Error: {{ error.message }}</span>
+    <!-- We can assume by this point that `isSuccess === true` -->
+    <ul v-else>
+      <li v-for="book in data" :key="book.id">{{ book.title }}</li>
+    </ul>
+    <button @click="onButtonClick">Add book</button>
+  </div>
 </template>
---
+++ src/api.ts
@@ -0,0 +1,25 @@
+const BASE_URL = "http://localhost:3000";
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  publishedYear: number;
+};
+
+export default {
+  books: {
+    list(): Promise<Book[]> {
+      return fetch(`${BASE_URL}/books`).then((response) => response.json());
+    },
+    add(book: Book): Promise<Book["id"]> {
+      return fetch(`${BASE_URL}/books`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      }).then((response) => response.json());
+    },
+  },
+};
